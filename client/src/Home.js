d592404a5703c9cce1bd9c39d433dfa9dd1c41e1
GrannyYetta <you@example.com>
2022-09-19 15:13:43 +0200

    frontend transfer

---
+++ client/src/Home.js
@@ -0,0 +1,14 @@
+
+import RecipesList from "./Components/RecipesList";
+import Banner from "./Components/Banner";
+
+function Home({recipes}) {
+  return (
+    <div className="home-style">
+      <Banner />
+      <RecipesList recipes={recipes} />
+    </div>
+  );
+}
+
+export default Home;
